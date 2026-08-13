06bc85aa97d42728ac2229c1be3a918dcbaa13fe
vmbresende-13 <vmbresende@gmail.com>
2026-08-13 20:03:33 -0300

    Refactor to implement Discord bot with message handling
    Removed the old slang dictionary and user input for adding new slang. Integrated Discord bot functionalities to respond to messages.

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
-import time
-import random
+import discord
+from bot_logic import gen_pass
 
-print("Bem vindo a biblioteca da definição das girias jovens")
+# A variável intents armazena as permissões do bot
+intents = discord.Intents.default()
+# Ativar a permissão para ler o conteúdo das mensagens
+intents.message_content = True
+# Criar um bot e passar as permissões
+client = discord.Client(intents=intents)
 
-time.sleep(1)
+print(gen_pass(10))
 
-gj = {
-    "Stalkear": "investigar a vida de alguém online",
-    "CRINGE": "algo vergonhoso ou constrangedor",
-    "VDD": "abreviação da palavra verdade",
-    "BISCOITAR": "postar algo apenas para chamar a atenção",
-    "HATER": "pessoa que está constantemente criticando os outros",
-    "VLW": "abreviação da palavra valeu",
-}
+@client.event
+async def on_ready():
+    print(f'Fizemos login como {client.user}')
 
-for giria in random.sample(list(gj.keys()), 5):
-    print(giria, "-", gj[giria])
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello!")
+    if message.content.startswith('$Como você está?'):
+        await message.channel.send("Estou bem, obrigado por perguntar!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
 
-time.sleep(1)
-
-ad = input("Deseja adicionar alguma outra giria? s / n ")
-
-if ad == "s":
-    nome = input("Digite a giria: ")
-    significado = input("Digite o significado: ")
-
-    gj[nome] = significado
-
-    print("Giria adicionada com sucesso!")
-    print(nome, "-", gj[nome])
-
-else:
-    print("Programa encerrado.")
-
-print("Girias sorteadas novamente:")
-
-for giria in random.sample(list(gj.keys()), 5):
-    print(giria, "-", gj[giria])
+client.run("MTUzNzU1MDgxNjE4ODAzOTE5OA.GLmXnC.ZB5FXnZrqAVM7D141qo9NpNxVCBYsUSmDJjX0E")
